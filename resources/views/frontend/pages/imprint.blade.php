e993a047de5c105bdca9087646ac535a0b3634a9
Marcel Stadelmann <m@marceli.to>
2022-07-12 19:03:32 +0200

    update

--- resources/views/frontend/pages/imprint.blade.php
+++ resources/views/frontend/pages/imprint.blade.php
@@ -5,7 +5,7 @@
     <article>
       <h1>Impressum</h1>
       <h2>Inhaber der Website</h2>
-      <p>APLEONA REAL ESTATE AG<br>Industriestrasse 21<br>8304 Wallisellen<br>T +41 44 878 77 77<br><a href="http://www.apleona.com" target="_blank" rel="noopener">www.apleona.com</a></p>
+      <p>Apleona Schweiz AG<br>Industriestrasse 21<br>8304 Wallisellen<a href="http://www.apleona.ch" target="_blank" rel="noopener">www.apleona.ch</a></p>
       <h2>Design und Entwicklung</h2>
       <p>Stoz Werbeagentur AG<br>Barzloostrasse 2<br>8330 Pfäffikon ZH<br>T + 41 44 950 35 50<br><a href="mailto:hello@stoz.ch">hello@stoz.ch</a><br><a href="https://www.stoz.ch" target="_blank" rel="noopener">www.stoz.ch</a></p>
       <h2>Programmierung</h2>
